d34dcf6ee8971b3951689129a3853e9cf0b686d0
danmachiweeb <doma.noobik@gmail.com>
2022-05-03 13:52:11 +0200

    slideshow

---
+++ parlaments.js
@@ -0,0 +1,21 @@
+
+window.onload = function(){
+  let slideIndex = 1;
+  showSlides(slideIndex);
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+  slideIndex++;
+  setTimeout(showSlides, 3000);
+}}
