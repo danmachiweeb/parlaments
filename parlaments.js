4cd9aadbbfde0f3f0cf4e370733e2e43c73d714b
danmachiweeb <doma.noobik@gmail.com>
2022-05-03 16:23:47 +0200

    slick slideshow

--- parlaments.js
+++ parlaments.js
@@ -1,21 +1,19 @@
 
+
 window.onload = function(){
   let slideIndex = 1;
   showSlides(slideIndex);
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("slides");
+  let slides = document.getElementsByClassName("slide");
   let dots = document.getElementsByClassName("dot");
-  if (slideIndex > slides.length) {slideIndex = 1} 
+  if (i > slides.length) {i = 1} 
+  if (slideIndex > slides.length){slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
-  }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
   slideIndex++;
-  setTimeout(showSlides, 3000);
+  setTimeout(showSlides, 3300);
 }}
